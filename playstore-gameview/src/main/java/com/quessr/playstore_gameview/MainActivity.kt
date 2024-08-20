65f5fcd9d94ccebb64a3c2bc09dd311932db1766
quessr <heosera76@gmail.com>
2024-08-20 16:15:55 +0900

    feat: MainActivity 레이아웃에 GameListView 추가 및 카드 타입에 따른 데이터 설정

--- playstore-gameview/src/main/java/com/quessr/playstore_gameview/MainActivity.kt
+++ playstore-gameview/src/main/java/com/quessr/playstore_gameview/MainActivity.kt
@@ -5,16 +5,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.quessr.playstore_gameview.model.GameListModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val bigImageGameListView = findViewById<GameListView>(R.id.bigImageGameListView)
+        val smallImageGameListView = findViewById<GameListView>(R.id.smallImageGameListView)
+
+        val bigImageGameList = listOf(
+            GameListModel.BigImageCard("1", "콜로사트론", "Best Developer", "https://example.com/image1.jpg"),
+            GameListModel.BigImageCard("2", "하늘 방어: 전쟁 의무", "Top Developer", "https://example.com/image2.jpg"),
+        )
+        val smallImageGameList = listOf(
+            GameListModel.SmallImageCard("3", "small 하늘 방어: 전쟁 의무", "Top Developer", "https://example.com/image2.jpg"),
+            GameListModel.SmallImageCard("4", "small 콜로사트론", "Top Developer", "https://example.com/image2.jpg")
+        )
+
+        bigImageGameListView.submitList(bigImageGameList)
+        smallImageGameListView.submitList(smallImageGameList)
     }
 }
