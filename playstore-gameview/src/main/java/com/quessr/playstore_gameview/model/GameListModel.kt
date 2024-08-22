906fd93692048d8d05f85de9f75b1c35263a7507
quessr <heosera76@gmail.com>
2024-08-22 15:55:32 +0900

    feat: GameListModel 클래스 필드 추가 및 수정

--- playstore-gameview/src/main/java/com/quessr/playstore_gameview/model/GameListModel.kt
+++ playstore-gameview/src/main/java/com/quessr/playstore_gameview/model/GameListModel.kt
@@ -7,20 +7,22 @@ sealed class GameListModel() {
         override val id: String,
         val title: String,
         val developer: String,
-        val url: String
+        val url: String,
+        val bannerUrl: String
     ) : GameListModel()
 
     data class SmallImageCard(
         override val id: String,
         val title: String,
-        val developer: String,
-        val url: String
+        val score: String,
+        val url: String,
     ) : GameListModel()
 
     data class ListCard(
         override val id: String,
         val title: String,
-        val developer: String,
-        val url: String
+        val category: String,
+        val score: String,
+        val url: String,
     ) : GameListModel()
 }
