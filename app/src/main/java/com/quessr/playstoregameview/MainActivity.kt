3f4ac9033fc8c4fcad1ca784489189dc0c1b7c96
quessr <heosera76@gmail.com>
2024-08-29 16:33:24 +0900

    feat: JSON 데이터 파싱 및 GameListView에 BigImageItem 목록 표시 기능 추가

--- app/src/main/java/com/quessr/playstoregameview/MainActivity.kt
+++ app/src/main/java/com/quessr/playstoregameview/MainActivity.kt
@@ -1,20 +1,48 @@
 package com.quessr.playstoregameview
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.gson.Gson
+import com.quessr.playstore_gameview.GameListView
+import com.quessr.playstore_gameview.model.GameItem
+import com.quessr.playstoregameview.data.datasource.response.GameResponse
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val jsonReader = assets.open("googleplay_game_action_30.json").reader()
+        val jsonString = jsonReader.readText()
+
+        Log.d("MainActivity", "jsonString : $jsonString")
+        jsonReader.close()
+
+        val gameResponse = Gson().fromJson(jsonString, GameResponse::class.java)
+        Log.d("MainActivity", "menu : $gameResponse")
+
+        val bigImageGameListView =
+            findViewById<GameListView>(com.quessr.playstore_gameview.R.id.bigImageGameListView)
+        val bigImageGameList = mutableListOf<GameItem.BigImageItem>()
+
+        gameResponse.content.forEachIndexed { index, gameItem ->
+            bigImageGameList.add(
+                GameItem.BigImageItem(
+                    id = gameItem.id.toString(),
+                    title = gameItem.title,
+                    developer = gameItem.developer,
+                    url = gameItem.url,
+                    bannerUrl = gameItem.url,
+                    categoryTitle = "액션 게임"
+                )
+            )
         }
+
+        bigImageGameListView.submitList(bigImageGameList)
+
     }
 }
