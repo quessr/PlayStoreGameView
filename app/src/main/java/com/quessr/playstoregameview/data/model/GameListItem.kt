03b1dfacc37ddf990306253ea52075a6481a0d6d
quessr <heosera76@gmail.com>
2024-09-05 11:08:12 +0900

    feat: GameListItem sealed class 구현 및 적용
    - Promo, ListChart, Featured 타입 추가

---
+++ app/src/main/java/com/quessr/playstoregameview/data/model/GameListItem.kt
@@ -0,0 +1,22 @@
+package com.quessr.playstoregameview.data.model
+
+import com.quessr.playstore_gameview.model.GameItem
+
+sealed class GameListItem {
+    abstract val id: String
+
+    data class Promo(
+        override val id: String,
+        val items: List<GameItem.BigImagePromoItem>
+    ) : GameListItem()
+
+    data class ListChart(
+        override val id: String,
+        val items: List<GameItem.ListItem>
+    ) : GameListItem()
+
+    data class Featured(
+        override val id: String,
+        val items: List<GameItem.BigImageFeaturedItem>
+    ) : GameListItem()
+}
--- app/src/main/java/com/quessr/playstoregameview/ui/gameList/viewholder/BaseGameListViewHolder.kt
+++ app/src/main/java/com/quessr/playstoregameview/ui/gameList/viewholder/BaseGameListViewHolder.kt
@@ -4,8 +4,9 @@ import android.content.Context
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import androidx.viewbinding.ViewBinding
 import com.quessr.playstore_gameview.model.GameItem
+import com.quessr.playstoregameview.data.model.GameListItem
 
-abstract class BaseGameListViewHolder<T : GameItem, VB : ViewBinding>(binding: VB) :
+abstract class BaseGameListViewHolder<T : GameListItem, VB : ViewBinding>(binding: VB) :
     ViewHolder(binding.root) {
 
     protected val context: Context = binding.root.context
